24bc2cb2145c70fd64e5f4f0ff8ecf3340241b2f
karuzelisZ <z.karuzelis@gmail.com>
2023-06-29 18:12:55 +0300

    finish 2.0

--- Baigiamas_done/content/kontaktai_cont.php
+++ Baigiamas_done/content/kontaktai_cont.php
@@ -14,6 +14,7 @@
             <h3>Saskaitos</h3>
                 <p>Kačių prieglaudos filijalo a/s: LT557300010130312940 (swedbank)</p>
                 <p>Šunų prieglaudos filijalo a/s: LT417300010152012189 (swedbank)</p>
+                
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9219.2065907923!2d25.2942469!3d54.7131128!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dd97d148ba7889%3A0x706351a497b2f703!2zVsWhxK4gTmFtaW51a2FpICJLYcSNacWzIE5hbWFpIg!5e0!3m2!1sen!2slt!4v1687721338340!5m2!1sen!2slt" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 </section>
